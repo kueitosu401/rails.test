780805e7268697c5229769d19533bae79401f779
EC2 Default User <tisikizinnrey@gmail.com>
2021-06-06 14:04:53 +0000

    6月6日7章まで

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -10,8 +10,8 @@ class TodolistsController < ApplicationController
     list = List.new(list_params)
     # ２. データをデータベースに保存するためのsaveメソッド実行
     list.save
-    # ３. トップ画面へリダイレクト
-    redirect_to '/top'
+   # 詳細画面へリダイレクト
+    redirect_to todolist_path(list.id)
   end
   
   
@@ -19,10 +19,23 @@ class TodolistsController < ApplicationController
    @lists = List.all
   end
   
+  def show
+   @list = List.find(params[:id])
+  
+  end 
+  
+  def edit
+    @list = List.find(params[:id])
+  end
+  
+  def update
+   list = List.find(params[:id])
+   list.update(list_params)
+   redirect_to todolist_path(list)
+  end
   
   private
-  # ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '5f098e1dc45375cf6745550ebdbddc0c12c9478d028712081dd91e741a2091e6e1e3fc3121ffdd3c90cff5da01681a92dad4d156a8b5d90657b3fd7b052033e7'
